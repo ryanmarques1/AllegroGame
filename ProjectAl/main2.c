8d685a70ab7983c50142224c2bf5c718bb655d69
sidemaze@ <47296650+Sidemaze@users.noreply.github.com>
2020-09-02 12:37:17 -0300

    2main
    ...

--- ProjectAl/main2.c
+++ ProjectAl/main2.c
@@ -5,10 +5,7 @@
 int main()
 {
     //Variaveis do Game;
-    const int largura = 640;
-    const int altura = 480;
-    const float x = 900;
-    const float y = 264;
+
     //Start Allegro5
     //This command create a screen
     ALLEGRO_DISPLAY* display=NULL;
@@ -29,31 +26,11 @@ int main()
 
     al_init_primitives_addon();
     /*Utilização de imagesn */  al_init_image_addon();
-    /*Dando load na imagem*/  ALLEGRO_BITMAP *piso = al_load_bitmap("teste.bmp");
-
-
-    /*
-    al_draw_triangle(10,200,100,10,190,200,al_map_rgb(255,0,255),2);
-    al_draw_pixel(300,250,al_map_rgb(200,255,255));
-    al_draw_line(100,100,largura - 100, 100, al_map_rgb(255,0,0),2);
-    al_draw_line(50,200,largura - 50, 200, al_map_rgb(0,255,0),2);
-    al_draw_filled_triangle(100,250,150,10,190,200,al_map_rgb(155,0,0));
-    al_draw_circle(250,200,50,al_map_rgb(0,0,255),2);
-    al_draw_ellipse(100,100,100,200,al_map_rgb(0,255,0),2);
-    al_draw_bitmap(piso,50,50,ALLEGRO_FLIP_HORIZONTAL);
-    */
-    float pontos[] = {0,0,400,100,50,200,largura,altura};
-    float pontos2[] = {0,0,largura,400,250,300,altura,largura};
-    float pontos3[] = {0,altura,300,350,largura,altura,500,100};
-    al_draw_spline(pontos,al_map_rgb(255,0,0),5);
-    al_draw_spline(pontos2,al_map_rgb(0,255,0),5);
-    al_draw_spline(pontos3,al_map_rgb(0,0,255),5);
 
     //-------------------------------------
     al_flip_display();
     al_clear_to_color(al_map_rgb(0,0,0));
     al_rest(5.0);
     al_destroy_display(display);
-    al_destroy_bitmap(piso);
     return 0;
 }
