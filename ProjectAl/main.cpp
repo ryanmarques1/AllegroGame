83d6ef0b8232e6f45d3c396bd54d920e588c38af
sidemaze@ <47296650+Sidemaze@users.noreply.github.com>
2020-09-07 15:29:49 -0300

    Movi

--- ProjectAl/main.cpp
+++ ProjectAl/main.cpp
@@ -6,11 +6,16 @@
 #include <allegro5/allegro_ttf.h>
 int main()
 {
+
+
     //display
     const int largura = 640;
     const int altura = 480;
+
     //Variaveis do Game;
     bool END = false;
+    int px = 100;
+    int py = 100;
     //Start Allegro5
     //This command create a screen
     ALLEGRO_DISPLAY* display=NULL;
@@ -28,8 +33,10 @@ int main()
 
 
 
-    //Inicialização de addons
+    //Inicialização de addons e instalações.
     al_install_keyboard();
+    al_init_primitives_addon();
+    al_init_image_addon();
     //importanado os atributos.
 
     ALLEGRO_EVENT_QUEUE *events = NULL;
@@ -45,16 +52,35 @@ int main()
     while(!END){
         ALLEGRO_EVENT ev;
         al_wait_for_event(events,&ev);
-        if(ev.type == ALLEGRO_EVENT_KEY_UP){
+        if(ev.type == ALLEGRO_EVENT_KEY_DOWN){
             if(ev.keyboard.keycode == ALLEGRO_KEY_ENTER){
                 END = true;
             }
+            switch(ev.keyboard.keycode){
+            case ALLEGRO_KEY_UP:
+                py-=20;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                py+=20;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                px+=20;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                px-=20;
+                break;
+            }
         }
-        if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+        else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
             END = true;
         }
-        al_flip_display();
+    //Drawn
+       al_draw_filled_rectangle(px,py, px + 10, py + 10, al_map_rgb(255,0,0));
+
+
+       al_flip_display();
     }
+
     //-------------------------------------
 
     al_destroy_display(display);
